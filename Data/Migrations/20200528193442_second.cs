7efbd73cfa314db4b6d3e24ebeb3164f8f86380f
RissaSams <rissa.sams@yahoo.com>
2020-05-29 08:51:37 -0500

    Add project files.

---
+++ Data/Migrations/20200528193442_second.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentRegistration.Data.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    StudentId = table.Column<int>(nullable: false),
+                    CellPhone = table.Column<double>(nullable: false),
+                    YearBorn = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistration.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Display(Name = "Last, First Name")]
+        public string Name { get; set; }        
+        [Display(Name = "Student ID")]
+        public int StudentId { get; set; }
+        [Display(Name = "Cell Phone")]
+        public double CellPhone { get; set; }
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [DisplayFormat(DataFormatString = "{0:somthing@email.com}", ApplyFormatInEditMode = true)]
+        public string EmailAddress { get; set; }
+        [Display(Name = "Birth Year")]
+        public int YearBorn { get; set; }
+        
+    }
+}
